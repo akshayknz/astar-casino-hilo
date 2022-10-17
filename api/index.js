aad775d7ceef029779db3aeb0771332d1d4dc0ae
Sasuke Uchiha <97214534+Sasuke031026@users.noreply.github.com>
2022-10-18 01:10:58 +1000

    Update index.js
    Co-Authored-By: tDev <95761247+tdev0809@users.noreply.github.com>

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require("./api_controller");
 
 module.exports = (router) => {
-    // User API
+    // User API with tdev0809
     router.post("/start-signal", User.StartSignal);
     router.post("/start-game", User.Gaming);
     router.post("/start-cashout", User.CashOut);
